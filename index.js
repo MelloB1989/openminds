f1f07008bfdff45999a44689de036d26c8d963e3
NangunooriAbhinav <abhinavnangunoori.29@gmail.com>
2024-01-23 19:33:44 +0530

    made responsive

--- index.js
+++ index.js
@@ -8,10 +8,10 @@
 //   });
 // });
 //
-// document.addEventListener("mousemove", function (e) {
-//   var xPos = e.pageX;
-//   var yPos = e.pageY;
-//   // console.log(xPos, yPos);
-//   document.querySelector(".circle-out").style.top = yPos - 15 + "px";
-//   document.querySelector(".circle-out").style.left = xPos - 16 + "px";
-// });
+document.addEventListener("mousemove", function (e) {
+  var xPos = e.pageX;
+  var yPos = e.pageY;
+  // console.log(xPos, yPos);
+  document.querySelector(".circle-out").style.top = yPos - 15 + "px";
+  document.querySelector(".circle-out").style.left = xPos - 16 + "px";
+});
