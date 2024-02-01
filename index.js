60ae3270b4a2344427a4352af0969d0578ceb76d
NangunooriAbhinav <abhinavnangunoori.29@gmail.com>
2024-02-01 22:41:26 +0530

    made changes to mouse pointer and svgs

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-// $(document).on("mousemove", function (e) {
+// document.addEventListener("mousemove", function (e) {
 //   var xPos = e.pageX;
 //   var yPos = e.pageY;
 //   // console.log(xPos, yPos);
-//   $(".circle-out").css({
-//     top: yPos,
-//     left: xPos,
-//   });
+//   document.querySelector(".circle-out").style.top = yPos - 69 + "px";
+//   document.querySelector(".circle-out").style.left = xPos - 15 + "px";
 // });
-//
 document.addEventListener("mousemove", function (e) {
+  var circleOut = document.querySelector(".circle-out");
   var xPos = e.pageX;
   var yPos = e.pageY;
-  // console.log(xPos, yPos);
-  document.querySelector(".circle-out").style.top = yPos - 69 + "px";
-  document.querySelector(".circle-out").style.left = xPos - 15 + "px";
+
+  // Subtract half the height and width of the circle to center it around the mouse pointer
+  var circleOffsetX = 40 / 2;
+  var circleOffsetY = 40 / 2;
+
+  // Adjust yPos to account for the nav bar height
+  var adjustedYPos = yPos - circleOffsetY - 70; // 75px is the height of the nav bar
+  var adjustedXPos = xPos - circleOffsetX;
+
+  circleOut.style.top = adjustedYPos + "px";
+  circleOut.style.left = adjustedXPos + "px";
 });
